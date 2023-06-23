0c3e20fab3dee8d12030cfa1db7f83b8e923f840
sacundim <anybody@sacundim.org>
2023-06-23 01:32:51 -0700

    Success! After setting up to run multiple rounds and doing both SVG and PNG output, I get it to crash.

--- README.md
+++ README.md
@@ -2,6 +2,36 @@ Simple example program to try and reproduce this issue with `vl-convert`:
 
 * https://github.com/vega/vl-convert/issues/67
 
-Prerequisite: Docker
+## Run in Docker
 
-To run: `./run-in-docker`
+```
+./run-in-docker
+```
+
+## Run standalone
+
+1. Create a venv and activate it
+2. `pip install -r requirements.txt`
+3. `vl-convert-bulk.py vega-lite/*.json`
+
+## Failure output seen
+
+```
+#
+# Fatal error in , line 0
+# Check failed: Deoptimizer::IsValidReturnAddress(PointerAuthentication::StripPAC(pc), isolate_).
+#
+#
+#
+#FailureMessage Object: 0xffff8cce8550
+==== C stack trace ===============================
+
+    /usr/local/lib/python3.9/site-packages/vl_convert/vl_convert.cpython-39-aarch64-linux-gnu.so(+0x11f9624) [0xffff8e015624]
+    /usr/local/lib/python3.9/site-packages/vl_convert/vl_convert.cpython-39-aarch64-linux-gnu.so(+0x910b24) [0xffff8d72cb24]
+    /usr/local/lib/python3.9/site-packages/vl_convert/vl_convert.cpython-39-aarch64-linux-gnu.so(+0x11f2820) [0xffff8e00e820]
+    /usr/local/lib/python3.9/site-packages/vl_convert/vl_convert.cpython-39-aarch64-linux-gnu.so(+0xe43d5c) [0xffff8dc5fd5c]
+    /usr/local/lib/python3.9/site-packages/vl_convert/vl_convert.cpython-39-aarch64-linux-gnu.so(+0x97ac54) [0xffff8d796c54]
+    /usr/local/lib/python3.9/site-packages/vl_convert/vl_convert.cpython-39-aarch64-linux-gnu.so(+0x977b2c) [0xffff8d793b2c]
+    /usr/local/lib/python3.9/site-packages/vl_convert/vl_convert.cpython-39-aarch64-linux-gnu.so(+0x1064234) [0xffff8de80234]
+Trace/breakpoint trap
+```
--- vl-convert-bulk.py
+++ vl-convert-bulk.py
@@ -1,28 +1,54 @@
 #!/usr/bin/env python3
 
+import argparse
 import gzip
 import logging
 import re
 import sys
 import vl_convert as vlc
 
+def process_arguments():
+    parser = argparse.ArgumentParser(description='Test program to reproduce vl-convert issue #67')
+    parser.add_argument('--rounds', type=int, default=6, help='How many rounds to run. Default: 6.')
+    parser.add_argument('filenames', type=str, metavar='FILENAME', nargs='+')
+    return parser.parse_args()
+
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-    for filename in sys.argv[1::]:
+    args = process_arguments()
+
+    logging.info("Will run %d rounds.", args.rounds)
+    for i in range(args.rounds):
+        logging.info("Running round %d...", i+1)
+        process_files(args.filenames)
+        logging.info("Done with round %d.", i+1)
+    logging.info("ALL DONE")
+
+def process_files(filenames):
+    for filename in filenames:
         logging.info("Processing %s...", filename)
         process_file(filename)
         logging.info("Processed %s.", filename)
-    logging.info("ALL DONE")
 
 def process_file(filename):
     basename = re.sub(r'\.json.gz$', '', filename)
-    svgname = f'{basename}.svg'
     with gzip.open(filename, 'rt') as i:
         spec = i.read()
-        with open (svgname, 'wt') as o:
-            logging.info('Writing to %s...', svgname)
-            svg = vlc.vegalite_to_svg(spec)
-            o.write(svg)
+        write_svg(spec, f'{basename}.svg')
+        write_png(spec, f'{basename}.png')
+
+def write_svg(spec, svgname):
+    with open(svgname, 'wt') as o:
+        logging.info('Writing to %s...', svgname)
+        svg = vlc.vegalite_to_svg(spec)
+        o.write(svg)
+
+def write_png(spec, pngname):
+    with open(pngname, 'wb') as o:
+        logging.info('Writing to %s...', pngname)
+        png = vlc.vegalite_to_png(spec)
+        o.write(png)
+
 
 if __name__ == "__main__":
     main()
